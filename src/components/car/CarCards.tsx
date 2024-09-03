1814576d20a9b289b306d16960e7b5ddd4e7d98e
alex2534 <alex2534@outlook.com>
2024-09-02 21:03:42 -0300

    Errors fixing

--- src/components/car/CarCards.tsx
+++ src/components/car/CarCards.tsx
@@ -84,6 +84,7 @@ export default function CarCards(data: ICardata) {
   });
   return (
     <div className={styles.container}>
+      <button onClick={getAllProducts}></button>
       <Row xs={1} md={2} className="g-4">
         {/* {Array.from({ length: 4 }).map((_, idx) => ( */}
         {allData !== undefined &&
