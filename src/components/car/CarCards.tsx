87f9b6debd744fb8333f0d3fa9280642fc659d30
alex2534 <alex2534@outlook.com>
2024-09-02 21:07:56 -0300

    Errors

--- src/components/car/CarCards.tsx
+++ src/components/car/CarCards.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
